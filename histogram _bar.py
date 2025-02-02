a07e58d13985858a50428408a93c54d7af58a369
Divya Pravin Patil <patildivya237@gmail.com>
2025-02-03 01:47:29 +0530

    Create histogram _bar.py

---
+++ histogram _bar.py
@@ -0,0 +1,31 @@
+
+
+import matplotlib.pyplot as plt
+
+import numpy as n
+
+from numpy import random
+
+
+
+x = random.randint(1, 100, size = (250))
+
+#y = random.randint(1, 10, size = (250))
+
+plt.hist(x)
+
+plt.show()
+
+
+
+
+
+#normal distribution histogram
+
+
+
+x = n.random.normal(170, 10, 250)
+
+plt.hist(x)
+
+plt.show()
